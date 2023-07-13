0777c42ef23d5ca488eaed16bd585f11ae60cd9f
arun patil <arunpatilmailbox@gmail.com>
2023-07-13 07:56:43 -0600

    Initial commit

---
+++ src/main/java/com/ap/profilingdemo/ProfilingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ap.profilingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ap/profilingdemo/ProfilingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.profilingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
